05a97256a596b6042303484d861cb3c5d3ec405f
miyako01 <91609350+miyako01@users.noreply.github.com>
2021-09-29 21:48:16 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -36,11 +36,11 @@ SERVER_ID = "96460cc1e778402xxxxxfe35fa97ce76"
 SECRET_KEY_NAME = "private_201xxxxx164308.key"
 
 # LOCAL ADDRESS
-LOCAL_ADDRESS = "https://[Heroku app name].herokuapp.com/"
+LOCAL_ADDRESS = "https://oumubotmiyako.herokuapp.com/"
 
 # DB config
-DB_HOST = "ec2-xxx-xxx-xxx-xxx.compute-1.amazonaws.com"
-DB_NAME = "x3mdxxxxsg5nx"
-DB_USER = "xsozxxxxvksikx"
+DB_HOST = "ec2-100-24-169-249.compute-1.amazonaws.com"
+DB_NAME = "d2ome06oqeu3mn"
+DB_USER = "udzxhqtnpyvrpe"
 DB_PORT = "5432"
-DB_PASSWORD = "xxxxxb3ff80c85d331da880f4b3e0xxxxxxxxxxxd552d64d128317653ce0xxxx"
+DB_PASSWORD = "00a28535846607bda6704cd35b685233338dda53b9901ad8bcc738cc80944548"
